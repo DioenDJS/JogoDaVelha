8f06932adcd6a3e0094cc1bfbb57ff003e870dab
Diovane Pacheti <negociospacheti@gmail.com>
2023-05-03 00:44:47 -0300

    :sparkles: create class service PlayService

---
+++ src/service/PlayService.java
@@ -0,0 +1,12 @@
+import entities.Play;
+import entities.Player;
+import entities.enums.PositionEnum;
+import utils.ConsoleColors;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class PlayService {
+
+
+}
