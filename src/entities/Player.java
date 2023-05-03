e9f7f1a6d2b668a614b117a896cb5581258f8128
Diovane Pacheti <negociospacheti@gmail.com>
2023-05-03 00:52:00 -0300

    :art: add line

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -36,7 +36,7 @@ public class Player {
     public String getColor() { return color; }
 
     public void setColor(String color) { this.color = color; }
-
+    
     @Override
     public String toString() {
         return "Player{" +
