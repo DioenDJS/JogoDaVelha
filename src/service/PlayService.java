dbc46ec45c9013a59b45d27117b17b71c983a52f
Diovane Pacheti <negociospacheti@gmail.com>
2023-05-03 00:46:54 -0300

    :sparkles: create method jogoEmProcesso

--- src/service/PlayService.java
+++ src/service/PlayService.java
@@ -8,5 +8,58 @@ import java.util.Scanner;
 
 public class PlayService {
 
+    public void jogoEmProgresso(Play play, List<Player> playerList, Scanner input){
+        int totalJogadas = 9;
+        boolean replayGame;
+        do {
+             play.getInitTabuleiro();
+
+            System.out.println(play.getToString());
+
+            for (int i = 0; i < totalJogadas; i++) {
+
+                int vezDoJogador = (i + 1) % 2 == 0 ? 1 : 0;
+
+                System.out.print("Jogador " + playerList.get(vezDoJogador).getColor() + (vezDoJogador + 1) + "º " + playerList.get(vezDoJogador).getName() + ConsoleColors.RESET + " escolha uma posição de 1 a 9 : ");
+                String escolha = input.nextLine();
+
+                if(!getMove(play, escolha, playerList.get(vezDoJogador).getSimbolo())) {
+                    i--;
+                    continue;
+                }
+
+                System.out.println(play.getToString());
+
+                String returnSimboloDoVencedor = play.getCheck(playerList.get(vezDoJogador).getSimbolo());
+
+                String[] simboloDoVencedor = returnSimboloDoVencedor.split(",");
+
+                if(returnSimboloDoVencedor.equals("")&& i < 8){
+                    continue;
+                }if(returnSimboloDoVencedor.equals("") && i == 8){
+                    System.out.println(ConsoleColors.YELLOW + "Deu velha!" + ConsoleColors.RESET);
+                    break;
+                } else if (simboloDoVencedor[3].equals("X") || simboloDoVencedor[3].equals("O")) {
+                    Player playerWinner = playerList.get(0).getSimbolo().equals(simboloDoVencedor[3]) ? playerList.get(0) : playerList.get(1);
+
+                    System.out.println("Jogador " + playerWinner.getColor() + playerWinner.getName() + ConsoleColors.RESET + " venceu!");
+                    String[] positionWinnewr = {simboloDoVencedor[0], simboloDoVencedor[1], simboloDoVencedor[2] };
+
+                    if(playerList.get(0).getSimbolo().equals(simboloDoVencedor[3])){
+                        play.getScoreboard().setPlayerOne(play.getScoreboard().getPlayerOne() + 1);
+                    }else{
+                        play.getScoreboard().setPlayerTwo(play.getScoreboard().getPlayerTwo() + 1);
+                    }
+
+                    System.out.println(play.getViewPalyerWinner(positionWinnewr));
+                    break;
+                }
+
+            }
+
+           replayGame = jogarNovamente(input);
+
+        }while (replayGame);
+    }
 
 }
