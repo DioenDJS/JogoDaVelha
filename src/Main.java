06710272f597f755a958407c2b33750dae949c29
Diovane Pacheti <negociospacheti@gmail.com>
2023-05-09 15:32:14 -0300

    :recycle: clear class Main

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,11 @@
-import entities.Scoreboard;
-import service.PlayService;
-import service.PlayerService;
-import utils.ConsoleColors;
-import entities.Play;
-import entities.Player;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import service.PlayStartService;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Player> playerList = new ArrayList<>();
-        Play play = new Play();
-        PlayerService playerService = new PlayerService();
-        PlayService playService = new PlayService();
+        PlayStartService start = new PlayStartService();
 
-        Scanner input = new Scanner(System.in);
-
-        playerList = playerService.criandoJogadores(playerList, input);
-
-        playService.jogoEmProgresso(play, playerList, input);
+        start.iniciarPartida();
         
     }
 }
