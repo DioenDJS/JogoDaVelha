4e3a51a99756e20d5217c488ddcf79f3447520b2
Diovane Pacheti <negociospacheti@gmail.com>
2023-05-24 00:47:41 -0300

    :fire: Remove method clear class

--- src/entities/Play.java
+++ src/entities/Play.java
@@ -31,7 +31,6 @@ public class Play {
 
     public void getInitTabuleiro(){
         int layoutNumericoTabuleiro = 1;
-        System.out.println(scoreboard.toString() + "\n");
         this.scoreboard.toString();
         for (int i = 0; i < this.tabuleiro.length; i++) {
             for (int j = 0; j < this.tabuleiro[i].length; j++) {
@@ -39,67 +38,5 @@ public class Play {
             }
         }
         setTabuleiro(this.tabuleiro);
-    }
-
-    public String getCheck(String sinal){
-
-
-         if((tabuleiro[0][0].equals(sinal) && tabuleiro[0][1].equals(sinal) && tabuleiro[0][2].equals(sinal))){
-            return  "00,01,02," + sinal;
-         }
-
-        if((tabuleiro[1][0].equals(sinal) && tabuleiro[1][1].equals(sinal) && tabuleiro[1][2].equals(sinal))){
-            return  "10,11,12," + sinal;
-        }
-
-        if((tabuleiro[2][0].equals(sinal) && tabuleiro[2][1].equals(sinal) && tabuleiro[2][2].equals(sinal))){
-            return  "20,21,22," + sinal;
-        }
-
-        if((tabuleiro[0][0].equals(sinal) && tabuleiro[1][0].equals(sinal) && tabuleiro[2][0].equals(sinal))){
-            return "00,10,20," + sinal;
-        }
-
-        if((tabuleiro[0][1].equals(sinal) && tabuleiro[1][1].equals(sinal) && tabuleiro[2][1].equals(sinal))){
-            return "01,11,21," + sinal;
-        }
-
-        if((tabuleiro[0][2].equals(sinal) && tabuleiro[1][2].equals(sinal) && tabuleiro[2][2].equals(sinal))){
-            return "02,12,22," + sinal;
-        }
-
-        if((tabuleiro[0][0].equals(sinal) && tabuleiro[1][1].equals(sinal) && tabuleiro[2][2].equals(sinal))){
-            return "00,11,22," + sinal;
-        }
-        if((tabuleiro[0][2].equals(sinal) && tabuleiro[1][1].equals(sinal) && tabuleiro[2][0].equals(sinal))){
-            return "02,11,20," + sinal;
-         }
-        return "";
-    }
-
-    //recebe a posição pra ser checada e o sinal
-    public String cpuChecaPosicaoVitoriaOuDerrota(String positionWinner, String sinal) {
-
-
-        //separa o valor na virgula
-        String[] positions = positionWinner.split(",");
-
-        //preserva o valor que havia na posição a ser checada
-        String guardaValor = tabuleiro[Integer.parseInt(positions[0])][Integer.parseInt(positions[1])];
-
-        //seta o valor para validar
-        setValueBoard(Integer.parseInt(positions[0]),Integer.parseInt(positions[1]), sinal);
-
-        //checando se o adiversirio vence
-        String existe = getCheck(sinal);
-
-        //retorna valor pra posição
-        setValueBoard(Integer.parseInt(positions[0]),Integer.parseInt(positions[1]), guardaValor);
-
-        if(existe.isEmpty()){
-           return "";
-        }
-
-        return positionWinner;
     }
 }
